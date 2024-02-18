65b8a35e17a2b876019269fbaf5936f86ccc94b7
IllidanHorns <illidanhorns13@gmail.com>
2024-02-18 16:00:48 +0300

    FirstVersion

---
+++ Pract3CPlus/Pract3CPlus.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <format>
+
+int main()
+{
+    float numbers[3][5];
+
+    for (int i; i < 3; i++)
+    {
+        std::cout << "Введиде значение строки номер - ";
+        std::cout << i + 1;
+        for (int j; j < 5; j++)
+        {
+            std::cin >> numbers[i][j];
+        }
+    }
+}
